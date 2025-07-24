67f2a0f22ffdb16cb5bb539452f509dc2ee15431
Danylo Namuenko <d.naumenko1111@gmail.com>
2025-07-24 18:33:57 +0200

    Deleted Notes as property in User domain model

--- Notebook.Domain/Models/User.cs
+++ Notebook.Domain/Models/User.cs
@@ -5,5 +5,4 @@ public record User
     public Guid Id { get; init; }
     public string Login { get; init; } = null!;
     public string PasswordHash { get; init; } = null!;
-    public ICollection<Note> Notes { get; init; } = new List<Note>();
 }
